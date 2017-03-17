1ff73cb0191424387609a3497d5c8384e5319fb5
Arek Mano <manoukian.arek@gmail.com>
2017-03-16 20:15:57 -0400

    made projects responsive

--- src/js/controllers/project_controller.js
+++ src/js/controllers/project_controller.js
@@ -3,10 +3,15 @@
 angular.module('PersonalApp').controller('ProjectController', function(ProjectService, Category){
   var vm = this;
   vm.categoryOptions = Category.all;
+  vm.selectedCategory = Category.all.all;
 
   vm.splitProjects = function(){
     vm.projects1 = ProjectService.projects.slice(0, (ProjectService.projects.length  + 1 ) / 2);
     vm.projects2 = ProjectService.projects.slice((ProjectService.projects.length  + 1 ) / 2, ProjectService.projects.length);
+  };
+
+  vm.openMenu = function($mdMenu, ev) {
+    $mdMenu.open(ev);
   };
 
   vm.clickCategory = function(category){
@@ -14,9 +19,10 @@ angular.module('PersonalApp').controller('ProjectController', function(ProjectSe
       cat.selected = false;
     });
     category.selected = true;
+    vm.selectedCategory = category;
     ProjectService.refreshProjects(category);
     vm.splitProjects();
   };
-  ProjectService.refreshProjects(vm.categoryOptions.all);
+  ProjectService.refreshProjects(vm.selectedCategory);
   vm.splitProjects();
 });
